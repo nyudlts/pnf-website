5901524a7d5e746b7425716ae44c116800d84d8b
Andy Miller <rhuk@mac.com>
2025-11-15 16:14:49 +0000

    footer + scroll to top fixes
    Signed-off-by: Andy Miller <rhuk@mac.com>

--- themes/nyu-lib/js/components/header-sticky.js
+++ themes/nyu-lib/js/components/header-sticky.js
@@ -12,3 +12,28 @@ function handleClick() {
 
 toggleOpen.addEventListener('click', handleClick);
 toggleClose.addEventListener('click', handleClick);
+
+document.addEventListener('DOMContentLoaded', function() {
+    var scrollToTopButton = document.getElementById('scroll-to-top');
+
+    if (scrollToTopButton) {
+        var toggleScrollToTopVisibility = function () {
+            if (window.scrollY > 200) {
+                scrollToTopButton.classList.add('visible');
+            } else {
+                scrollToTopButton.classList.remove('visible');
+            }
+        };
+
+        toggleScrollToTopVisibility();
+
+        window.addEventListener('scroll', toggleScrollToTopVisibility, { passive: true });
+
+        scrollToTopButton.addEventListener('click', function () {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        });
+    }
+});
